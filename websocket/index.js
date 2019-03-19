a930dad3ab0be246c3ec9fb46264b5458a88dd5d
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-19 10:55:44 -0300

    [UPDATE] Ajustes para que o container seja gerado e executado corretamente.
    #155

--- websocket/index.js
+++ websocket/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const app = express();
-let server = http.createServer(app);
+server = http.createServer(app);
 const io = socketio(server);
 
 const clients = [];
@@ -14,29 +14,31 @@ app.use(
 /**
  * Initialize Server
  */
-server.listen(8001, function() {
+server.listen(8001, function () {
     console.log("Servidor Rodando na Porta 8001");
 });
 
 /**
  * Página de Teste
  */
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.send("Servidor Rodando...");
 });
 
 // Recebe requisição do Laravel
-app.post("/like", function(req, res) {
+app.post("/like", function (req, res) {
     var params = req.body;
     var clients = io.sockets.clients().sockets;
-    for (const key in clients) {
-        if (key != params.id) clients[key].emit("like", params);
+    if (clients.length > 0) {
+        Object.keys(clients).forEach((indice) => {
+            if (indice != params.id) clients[indice].emit("like", params);
+        });
     }
     res.send();
 });
 
 // Recebe conexão dos usuários no servidor
-io.on("connection", function(client) {
+io.on("connection", function (client) {
     // Adicionado clientes
     client.emit("welcome", {
         id: client.id
