251d1f22a48d678c2952f09f22018f938eb558a7
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-26 13:33:14 -0300

    [UPDATE] Modificando dependências do serviço
    #155 #169

--- websocket/index.js
+++ websocket/index.js
@@ -5,6 +5,7 @@ const http = require("http");
 const app = express();
 let server = http.createServer(app);
 const io = socketio(server);
+require('dotenv').config()
 const env = require('env')()
 const jsonwebtoken = require('jsonwebtoken');
 
@@ -27,57 +28,58 @@ app.get("/", function (req, res) {
 });
 
 io.on("connection", function (socket) {
-    
+
     console.log('an user connected');
-    
+
     clients.push({clientId: socket.client.id})
-    
+
     io.emit('connectedUsers', clients.length);
-    
+
     socket.on('serverEntrarEmCanal', (dados) => {
-        // canaisDeSistemas[dados]
-    
         try {
             const canal = dados.sistema_id
             const token = dados.token;
-            const tokenDecodificada = jsonwebtoken.verify(token, env.get('JWT_SECRET'))
+            const tokenDecodificada = jsonwebtoken.verify(token, process.env.JWT_SECRET)
             const sistemas = tokenDecodificada['sistemas'];
-    
+
             const indice = sistemas.findIndex(sistema => sistema.sistema_id === canal);
             if(indice === -1) {
                 throw "Sistema solicitado não faz parte do grupo de permissões do usuário.";
             }
-            
+
             const canalPesquisado = canaisDeSistemas.find(valor => valor == canal);
             if(canalPesquisado != canal) {
                 canaisDeSistemas.push(canal);
                 socket.join(canal);
+                socket.to(canal).emit('clientEntrarCanal', dados)
             }
-            
+
         } catch(Exception) {
             console.log(Exception)
         }
-        
     });
-    
-    
-    
+
+    socket.on('serverMensagem', (dados) => {
+        const canal = dados.sistema_id;
+        socket.to(canal).emit('clientMensagem', dados);
+    });
+
     socket.on('disconnect', () => {
         const index = clients.findIndex(client => client.clientId === socket.client.id);
         clients.splice(index, 1);
         console.log('user disconnected');
         io.emit('connectedUsers', clients.length);
-        io.emit('clientDisconnected', {clientId: socket.client.id});
     });
-    
-    socket.on('serverGlobalMessage', (dados) => {
-        //clientSystem
+
+    socket.on('serverMensagemGlobal', (dados) => {
         console.log(`[${dados.clientId}]: ` + dados.message);
 
-        io.emit('clientGlobalMessage', dados);
+        canaisDeSistemas.forEach((value, index) => {
+            socket.to(value).emit('clientMensagemGlobal', dados);
+        });
     });
-    
-    
+
+
 });
 
 
