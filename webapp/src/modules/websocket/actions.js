cffdfa5374414a20aae74eb8ded49ef8490fec13
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-01 18:45:32 -0300

    [UPDATE] Criação de action, mutation e type para **Socket_serverEntrarEmSala**
    #155 #169

--- webapp/src/modules/websocket/actions.js
+++ webapp/src/modules/websocket/actions.js
@@ -1,4 +1,5 @@
 import * as types from './types';
+import $socket from './_helpers/socket-client-instance';
 
 export const Socket_connect = ({ commit }, data) => {
     commit(types.SOCKET_CONNECT, data);
@@ -12,6 +13,14 @@ export const Socket_clientConnectedUsers = ({ commit }, data) => {
     commit(types.SOCKET_CLIENT_CONNECTEDUSERS, data);
 };
 
+export const Socket_serverEntrarEmSala = ({ commit, socket }, data) => {
+    const { sala } = data.sala;
+    $socket.emit('serverEntrarEmSala', {
+        sala,
+    });
+    commit(types.SOCKET_SERVER_ENTRAREMSALA, data);
+};
+
 export const Socket_clientEntrarEmSala = ({ commit, socket }, data) => {
     commit(types.SOCKET_CLIENT_ENTRAREMSALA, data);
 };
@@ -20,4 +29,6 @@ export const Socket_clientSairDaSala = ({ commit, socket }, data) => {
     commit(types.SOCKET_CLIENT_SAIRDASALA, data);
 };
 
-
+export const Socket_clientMensagemSala = ({ commit, socket }, data) => {
+    commit(types.SOCKET_CLIENT_MENSAGEMSALA, data);
+};
--- webapp/src/modules/websocket/mutations.js
+++ webapp/src/modules/websocket/mutations.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import * as types from './types';
+import { websocket } from './getters';
 
 export const mutations = {
     [types.SOCKET_CONNECT](state, data) {
@@ -12,6 +13,11 @@ export const mutations = {
 
     [types.SOCKET_CLIENT_CONNECTEDUSERS](state, data) {
         state.websocket.connectedUsers = data;
+    },
+
+    [types.SOCKET_SERVER_ENTRAREMSALA](state, data) {
+        console.log('entrando na mutation SOCKET_SERVER_ENTRAREMSALA');
+        console.log(data);
     },
 
     [types.SOCKET_CLIENT_ENTRAREMSALA](state, data) {
@@ -31,8 +37,8 @@ export const mutations = {
                     },
                 ],
             });
+            state.salaAtual = sala;
         }
-        state.salaAtual = sala;
     },
 
     [types.SOCKET_CLIENT_SAIRDASALA](state, data) {
@@ -42,6 +48,23 @@ export const mutations = {
 
         if (indice !== -1) {
             state.websocket.salas.splice(indice, 1);
+        }
+    },
+
+    [types.SOCKET_CLIENT_MENSAGEMSALA](state, data) {
+        console.log('entrando na mutation SOCKET_CLIENTMENSAGEMSALA');
+        const { sala } = data;
+        const { usuario } = data;
+        const { mensagem } = data;
+        const horario = moment().format();
+        const indice = state.websocket.salas.findIndex(valor => valor.sala === sala);
+        if (indice !== -1) {
+            websocket.salas[indice].mensagens.push({
+                mensagem,
+                horario,
+                usuario,
+            });
+            state.salaAtual = sala;
         }
     },
 };
