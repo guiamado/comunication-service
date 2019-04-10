08e6e92d1ed462ecb5eda562e81a87fa6f44bb8e
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-10 17:24:57 -0300

    [FIX] Obtenção do cabeçalho que define se possui token ou não para todos os verbos utilizados nas requisições do frontend.
    #207

--- webapp/src/modules/account/_auxiliares/requisicao-autorizada.js
+++ webapp/src/modules/account/_auxiliares/requisicao-autorizada.js
@@ -1,29 +1,32 @@
 import axios from 'axios';
 import { obterCabecalhoComToken } from './jwt';
 
-const cabecalho = obterCabecalhoComToken();
-
 function post(urlString, data, config) {
+    const cabecalho = obterCabecalhoComToken();
     const mergedConfig = Object.assign(cabecalho, config);
     return axios.post(urlString, data, mergedConfig);
 }
 
 function get(urlString, config) {
+    const cabecalho = obterCabecalhoComToken();
     const mergedConfig = Object.assign(cabecalho, config);
     return axios.get(urlString, mergedConfig);
 }
 
 function remove(urlString, config) {
+    const cabecalho = obterCabecalhoComToken();
     const mergedConfig = Object.assign(cabecalho, config);
     return axios.delete(urlString, mergedConfig);
 }
 
 function put(urlString, data, config) {
+    const cabecalho = obterCabecalhoComToken();
     const mergedConfig = Object.assign(cabecalho, config);
     return axios.put(urlString, data, mergedConfig);
 }
 
 function patch(urlString, data, config) {
+    const cabecalho = obterCabecalhoComToken();
     const mergedConfig = Object.assign(cabecalho, config);
     return axios.patch(urlString, data, mergedConfig);
 }
