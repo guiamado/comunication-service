08f23f335d26834e1741035750d9548b56f04f9e
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-10 15:38:28 -0300

    [UPDATE] Padronização em português : Chamadas dos Types do módulo **account**.
    #126

--- webapp/src/modules/account/actions.js
+++ webapp/src/modules/account/actions.js
@@ -5,7 +5,7 @@ import { obterInformacoesJWT } from './_auxiliares/jwt';
 import { requisicaoAutorizada } from './_auxiliares/requisicao-autorizada';
 
 export const login = ({ dispatch, commit }, { email, password }) => {
-    commit(types.LOGINREQUEST, { email });
+    commit(types.LOGIN_REQUISICAO, { email });
 
     return requisicaoAutorizada.post(
         'http://localhost/v1/autenticacao/login',
@@ -15,13 +15,13 @@ export const login = ({ dispatch, commit }, { email, password }) => {
             if (response.data && response.data.data) {
                 const { data } = response.data;
                 if (data && data.token) {
-                    commit(types.LOGINSUCCESS, data.token);
+                    commit(types.LOGIN_SUCESSO, data.token);
                     dispatch('alert/info', 'Login realizado com sucesso!', {
                         root: true,
                     });
 
                     const objetoJWT = obterInformacoesJWT();
-                    commit(types.SETACCOUNTINFO, objetoJWT.user);
+                    commit(types.DEFINIR_INFORMACOES_CONTA, objetoJWT.user);
                 } else {
                     dispatch('alert/error', 'Falha ao realizar login.', {
                         root: true,
@@ -35,7 +35,7 @@ export const login = ({ dispatch, commit }, { email, password }) => {
         }
     }).catch((error) => {
         if (error.response && error.response.data) {
-            commit(types.LOGINFAILURE, error.response.data.error);
+            commit(types.LOGIN_FALHA, error.response.data.error);
             dispatch('alert/error', `Erro: ${error.response.data.error}`, {
                 root: true,
             });
@@ -49,16 +49,16 @@ export const logout = ({ commit }) => {
 };
 
 export const registrar = ({ dispatch, commit }, user) => {
-    commit(types.REGISTERREQUEST);
+    commit(types.REGISTRAR_REQUISICAO);
     axios.post('http://localhost/v1/conta', JSON.parse(JSON.stringify(user))).then(
         () => {
-            commit(types.REGISTERSUCCESS);
+            commit(types.REGISTRAR_SUCESSO);
             dispatch('alert/success', 'Cadastro realizado com sucesso!', { root: true });
             router.push('/login');
         },
         (error) => {
             if (error.response && error.response.data) {
-                commit(types.REGISTERFAILURE);
+                commit(types.REGISTRAR_FALHA);
                 dispatch('alert/error', error.response.data.error, {
                     root: true,
                 });
--- webapp/src/modules/account/mutations.js
+++ webapp/src/modules/account/mutations.js
@@ -1,19 +1,19 @@
 import * as types from './types';
 
 export default {
-    [types.LOGINREQUEST](state, token) {
+    [types.LOGIN_REQUISICAO](state, token) {
         state.status = { loggingIn: true };
         state.token = token;
     },
-    [types.LOGINSUCCESS](state, token) {
+    [types.LOGIN_SUCESSO](state, token) {
         localStorage.setItem('token', token);
         state.status = { loggedIn: true };
         state.token = token;
     },
-    [types.SETACCOUNTINFO](state, accountInfo) {
+    [types.DEFINIR_INFORMACOES_CONTA](state, accountInfo) {
         state.accountInfo = accountInfo;
     },
-    [types.LOGINFAILURE](state) {
+    [types.LOGIN_FALHA](state) {
         state.status = {};
         state.token = null;
     },
@@ -21,16 +21,14 @@ export default {
         state.status = {};
         state.token = null;
         state.accountInfo = null;
-        state.message = null;
-        state.message_type = null;
     },
-    [types.REGISTERREQUEST](state) {
+    [types.REGISTRAR_REQUISICAO](state) {
         state.status = { registrando: true };
     },
-    [types.REGISTERSUCCESS](state) {
+    [types.REGISTRAR_SUCESSO](state) {
         state.status = {};
     },
-    [types.REGISTERFAILURE](state) {
+    [types.REGISTRAR_FALHA](state) {
         state.status = {};
     },
 };
