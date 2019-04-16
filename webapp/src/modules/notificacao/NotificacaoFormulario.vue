8fc4201f7c7228cad89c5da6b28ee044a653c7b5
guiamado <amado.silva133@gmail.com>
2019-04-16 10:55:29 -0300

    [FIX] Corrige getter que estava faltando no componente para fazer o filtro das mensagens corretamente

--- webapp/src/modules/notificacao/NotificacaoFormulario.vue
+++ webapp/src/modules/notificacao/NotificacaoFormulario.vue
@@ -88,6 +88,7 @@ export default {
     computed: {
         ...mapGetters({
             mensagens: 'mensagem/mensagens',
+            informacoesConta: 'account/informacoesConta',
         }),
     },
     watch: {
