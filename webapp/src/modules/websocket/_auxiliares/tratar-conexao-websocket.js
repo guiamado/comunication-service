b415250077366e7a8ef54d69611a2631becd33b6
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-29 19:55:19 -0300

    [refactor] ajustes de lint
    Co-authored-by: Cleber Santos <oclebersantos@gmail.com>

--- webapp/src/modules/websocket/_auxiliares/tratar-conexao-websocket.js
+++ webapp/src/modules/websocket/_auxiliares/tratar-conexao-websocket.js
@@ -1,15 +1,13 @@
 import { obterInformacoesJWT } from '../../account/_auxiliares/jwt';
 
 export const tratarConexaoWebsocket = (store) => {
-    
     const token = localStorage.getItem('token');
-    if (store._vm.$socket.disconnected === true && token != null) {
+    const vm = store._vm;
+    if (vm.$socket.disconnected === true && token != null) {
         const informacoesToken = obterInformacoesJWT();
         if (informacoesToken !== '') {
-            console.log('somente 1 vez')
-            store._vm.$socket.io.opts.query.token = token;
-            store._vm.$socket.open();
-            console.log(store._vm.$socket);
+            vm.$socket.io.opts.query.token = token;
+            vm.$socket.open();
         }
     }
 };
