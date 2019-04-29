61f22e89d8ba542292906a06bda96b862fe235d0
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-29 19:38:33 -0300

    [UPDATE] Criação do método auxiliar 'tratar-conexao-websocket.js'
    Co-authored-by: Cleber Santos <oclebersantos@gmail.com>

---
+++ webapp/src/modules/websocket/_auxiliares/tratar-conexao-websocket.js
@@ -0,0 +1,14 @@
+import { obterInformacoesJWT } from '../../account/_auxiliares/jwt';
+
+export const tratarConexaoWebsocket = (store) => {
+    
+    const token = localStorage.getItem('token');
+    if (store._vm.$socket.disconnected === true && token != null) {
+        const informacoesToken = obterInformacoesJWT();
+        if (informacoesToken !== '') {
+            store._vm.$socket.io.opts.query.token = token;
+            store._vm.$socket.open();
+            console.log(store._vm.$socket);
+        }
+    }
+};
