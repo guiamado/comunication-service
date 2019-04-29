2c265ba0feb1d012c1bcc174cc1defb1be643525
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-29 19:00:24 -0300

    [UPDATE] Geração do pacote 0.0.11
    Co-authored-by: Cleber Santos <oclebersantos@gmail.com>

--- webapp/src/modules/account/actions.js
+++ webapp/src/modules/account/actions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import router from '../../router';
 import * as types from './types';
 import { obterInformacoesJWT } from './_auxiliares/jwt';
 import { requisicaoAutorizada } from './_auxiliares/requisicao-autorizada';
