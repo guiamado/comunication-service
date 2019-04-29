db23fe55985b95e1e12b7336b225eaeacf5a2db5
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-29 11:46:28 -0300

    [UPDATE] Ajustes para publicação do pacote 0.0.7

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import account from '@/modules/account';
-import alert from '@/modules/alert';
-import conta from '@/modules/conta';
-import plataforma from '@/modules/plataforma';
-import sistema from '@/modules/sistema';
-import mensagem from '@/modules/mensagem';
-import notificacao from '@/modules/notificacao';
-import websocket from '@/modules/websocket';
+import account from './modules/account';
+import alert from './modules/alert';
+import conta from './modules/conta';
+import plataforma from './modules/plataforma';
+import sistema from './modules/sistema';
+import mensagem from './modules/mensagem';
+import notificacao from './modules/notificacao';
+import websocket from './modules/websocket';
 
 Vue.use(Vuex);
 
