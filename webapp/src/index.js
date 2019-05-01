3e6c4cb9cd769996023a295ad48a8eee8fee02b5
vinnyfs89 <viniciusfesil@gmail.com>
2019-05-01 02:47:43 -0300

    [UPDATE] Definição de componentes a serem expostos.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,9 +5,16 @@ import plataforma from './modules/plataforma';
 import sistema from './modules/sistema';
 import websocket from './modules/websocket';
 
+import ComunicationServiceChat from './modules/websocket/Chat.vue';
+import ComunicationServiceStatus from './modules/websocket/Status.vue';
+import ComunicationServiceNotificacaoBadge from './modules/notificacao/NotificacaoBadge.vue';
+
 function install(Vue, { store }) {
     if (!store) console.log('Informe uma store.');
 
+    Vue.component('comunication-service-chat', ComunicationServiceChat);
+    Vue.component('comunication-service-status', ComunicationServiceStatus);
+    Vue.component('comunication-service-notificacao-badge', ComunicationServiceNotificacaoBadge);
 
     store.registerModule('account', account);
     store.registerModule('mensagem', mensagem);
