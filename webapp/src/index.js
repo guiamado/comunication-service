4cfce6daa15324c776b448a34a6bd1756e12d14a
vinnyfs89 <viniciusfesil@gmail.com>
2019-05-01 20:50:41 -0300

    [UPDATE] Tentativa de trabalhar com componentes de forma global e geração de versão 0.0.12.
    Referência: https://br.vuejs.org/v2/cookbook/packaging-sfc-for-npm.html

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -10,6 +10,9 @@ import ComunicationServiceStatus from './modules/websocket/Status.vue';
 import ComunicationServiceNotificacaoBadge from './modules/notificacao/NotificacaoBadge.vue';
 
 function install(Vue, { store }) {
+    if (install.installed) return;
+    install.installed = true;
+
     if (!store) console.log('Informe uma store.');
 
     Vue.component('comunication-service-chat', ComunicationServiceChat);
@@ -24,6 +27,26 @@ function install(Vue, { store }) {
     store.registerModule('websocket', websocket);
 }
 
+// Cria a definição do módulo para Vue.use()
+const plugin = {
+    install,
+};
+
+// Auto-instala quando o Vue é encontrado (no navegador via <script>)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+// Para permitir o uso como um módulo exportável (npm/webpack/etc.)
 export default {
     install,
+    ComunicationServiceChat,
+    ComunicationServiceStatus,
+    ComunicationServiceNotificacaoBadge,
 };
