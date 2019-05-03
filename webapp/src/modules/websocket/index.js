b2e8d6d7dd64e3d137d10c30548b972753e7e534
vinnyfs89 <viniciusfesil@gmail.com>
2019-05-03 16:54:24 -0300

    [REFACTOR] Ajuste de definição de plugins.

--- webapp/src/modules/websocket/index.js
+++ webapp/src/modules/websocket/index.js
@@ -2,7 +2,7 @@ import * as actions from './actions';
 import * as getters from './getters';
 import { mutations } from './mutations';
 import state from './state';
-import plugins from './plugins';
+import { plugins } from './plugins';
 
 export default {
     namespaced: true,
