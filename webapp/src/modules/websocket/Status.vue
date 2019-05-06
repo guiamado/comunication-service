e24bd3fc875bb7588d0cb0e69abedd9cd1629931
vinnyfs89 <viniciusfesil@gmail.com>
2019-05-06 14:50:46 -0300

    [UPDATE] Definição de props 'token' como opcional buscando por padrão do localStorage.

--- webapp/src/modules/websocket/Status.vue
+++ webapp/src/modules/websocket/Status.vue
@@ -37,16 +37,38 @@
 
 <script>
 
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import WebSocketMixins from './_auxiliares/mixins';
 
 export default {
     name: 'Chat',
     mixins: [WebSocketMixins],
+    props: {
+        token: {
+            type: String,
+            default: localStorage.getItem('token'),
+        },
+    },
     computed: {
         ...mapGetters({
             status: 'comunicationAccount/status',
             isConnected: 'comunicationWebsocket/isConnected',
+            informacoesConta: 'comunicationAccount/informacoesConta',
+        }),
+    },
+    watch: {
+        token(valor) {
+            localStorage.setItem('token', valor);
+        },
+    },
+    mounted() {
+        if (this.informacoesConta == null || this.informacoesConta.email == null) {
+            this.definirInformacoesConta(this.token);
+        }
+    },
+    methods: {
+        ...mapActions({
+            definirInformacoesConta: 'comunicationAccount/definirInformacoesConta',
         }),
     },
 };
