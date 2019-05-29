46d1e9c5bc5a4255f39a951c9471a86c01bcf295
guiamado <amado.silva133@gmail.com>
2019-05-29 10:40:09 -0300

    [UPDATE] Verificando se o usuario de teste foi criado

--- api/routes/web.php
+++ api/routes/web.php
@@ -26,6 +26,8 @@ $router->group(['prefix' => $apiPattern], function () use ($router) {
         ]
     );
 
+    $router->get('/acount[/{id}]', 'ContaController@get');
+
     $router->post('/recuperarSenha', 'RecuperarSenhaController@post');
 
     $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
